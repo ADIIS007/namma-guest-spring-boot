e8b34caa64a103ac740dd1c48c7793e08f8e1212
Athithya Ithayan <akilanathithyaithayan@gmail.com>
2024-10-07 00:35:49 +0530

    added some comments & steps to work on user side API

--- src/main/java/com/project/namma_guest/controller/AuthenticationController.java
+++ src/main/java/com/project/namma_guest/controller/AuthenticationController.java
@@ -15,22 +15,42 @@ public class AuthenticationController {
     AuthenticationController(AuthenticationHelper authenticationHelper) {
         this.authenticationHelper = authenticationHelper;
     }
+
+    //This function is to check weather the microservice is working or not
     @GetMapping("/auth-health")
     public ResponseEntity<String> health() {
         return ResponseEntity.ok("OK Auth Telling You");
     }
+
+    //Send OTP to the user to login
     @PostMapping("/send-otp")
     public ResponseEntity<String> sendOtp(@RequestBody Email email) {
-        log.info("Sending OTP to " + email);
+        log.info("Sending OTP to {}", email);
+        // Step 1 - Check the email is valid (must have @ ect) - 400 Bad Request
+        // Step 2 - Check that there is a user with the following - 404 Not Found
+        // Step 3 - OTP was not sent at lest 2 min ago - 429 Too Many Requests
+        // Step 4 - SEND OTP & time before it cant resend - 200 OK
         return authenticationHelper.sendOtp(email.getEmail());
     }
+
+    //Verify the OTP sent to a specific Email
     @PostMapping("/verify-otp")
     public ResponseEntity<String> verifyOtp(@RequestBody VerifyOTP verifyOTP) {
+        // Step 1 - Check the email is valid (must have @ ect) - 400 Bad Request
+        // Step 2 - Check that there is a user with the following - 404 Not Found
+        // Step 3 - OTP was sent at lest 2 min ago - 410 Gone/400 Bad Request
+        // Step 4 - SEND OTP & time before it cant resend - 200 OK
         return authenticationHelper.verifyOtp(verifyOTP.getEmail(), verifyOTP.getOtp());
     }
+
+    // This should re-generate the email and
     @PostMapping("/resend-otp")
     public ResponseEntity<String> resendOtp(@RequestBody Email email) {
-        log.info("Sending OTP to " + email);
+        log.info("resending OTP to {}", email);
+        // Step 1 - Check the email is valid (must have @ ect) - 400 Bad Request
+        // Step 2 - Check that there is a user with the following - 404 Not Found
+        // Step 3 - OTP was not sent at lest 2 min ago - 429 Too Many Requests
+        // Step 4 - SEND OTP & time before it cant resend again - 200 OK
         return authenticationHelper.sendOtp(email.getEmail());
     }
 }
--- src/main/java/com/project/namma_guest/controller/HomeController.java
+++ src/main/java/com/project/namma_guest/controller/HomeController.java
@@ -2,10 +2,9 @@ package com.project.namma_guest.controller;
 
 import com.project.namma_guest.model.PayingGuest;
 import com.project.namma_guest.service.PayingGuestService;
+import jakarta.websocket.server.PathParam;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,40 +15,56 @@ public class HomeController {
     public HomeController(PayingGuestService payingGuestService) {
         this.payingGuestService = payingGuestService;
     }
+
+    // Test weather the microservice is up or not
     @GetMapping("/test")
     public ResponseEntity<String> test() {
         return ResponseEntity.ok("Welcome to Namma Guest this is a test API!");
     }
-    @GetMapping("/list-guests")
-    public ResponseEntity<List<PayingGuest>> listGuests() {
-        return ResponseEntity.ok(payingGuestService.getAllPayingGuest());
-    }
-    @PostMapping("/list-pgs")
-    public ResponseEntity<List<PayingGuest>> listPGsByDistance(double longitude, double latitude, double distance) {
+
+    // Get list of PGs within a given distance from the given coordinates
+    @PostMapping("/list-pgs-within-distance")
+    public ResponseEntity<List<PayingGuest>> listPGsByDistance(
+            @RequestParam double longitude,
+            @RequestParam double latitude,
+            @RequestParam(defaultValue = "5.0") double distance) {
         //TODO: Implement this method & return the list of PGs within the given distance
+        // Step 1 - Check the latitude and longitude
+        // Step 2 - Get the list of PGs within the given distance
+        // Step 3 - Return the list
         List<PayingGuest>temp=new ArrayList<PayingGuest>();
         return ResponseEntity.ok(temp);
-        //TODO: Implement this method & return the list of PGs within the given distance
     }
-    @PostMapping("/about-pg")
-    public ResponseEntity<PayingGuest> aboutPg(double longitude, double latitude, double distance) {
+
+    // Get details of a given paying guest
+    @GetMapping("/about-pg/{id}")
+    public ResponseEntity<PayingGuest> aboutPg(@PathVariable String id) {
         //TODO: Implement this method & return the details about a PG
+        // Step 1 - Check the payingGuestId if not exist - 404 NOT FOUND
+        // Step 2 - Get the details of the PG weather visibility is public or not - 403 FORBIDDEN
+        // Step 3 - Return the details
         PayingGuest temp=new PayingGuest();
         return ResponseEntity.ok(temp);
     }
-    @PostMapping("/my-profile")
-    public ResponseEntity<String> myProfile() {
+
+    //load all the data regarding the user
+    @GetMapping("/my-profile/{id}")
+    public ResponseEntity<String> myProfile(@PathVariable String id) {
         //TODO: Implement this method & return the details about the user
+        // Step 1 - Get the user details using Email (later from the JWT token) - 404 Not Found
+        // Step 2 - If user not found - return 404 Not Found status code
+        // Step 3 - Return the details - 200 OK
         return ResponseEntity.ok("My Profile");
     }
-    @PostMapping("/pg-location")
-    public ResponseEntity<String> pgLocation() {
-        //TODO: Implement this method & return the locations of the PG for map
-        return ResponseEntity.ok("PG Location");
-    }
-    @PostMapping("/pg-booking")
-    public ResponseEntity<String> pgBooking() {
-        //TODO: Implement this method & return the booking details
-        return ResponseEntity.ok("PG Booking");
+
+    //Modify the User with respect to the user request
+    @PutMapping("/my-profile")
+    public ResponseEntity<String> updateMyProfile(){
+        //TODO: Implement this method & return the details about the user
+        // Step 1 - Get the user details using Email (later from the JWT token) - 404 Not Found
+        // Step 2 - Validate the request (input validation, data format etc.) - 400 Bad Request
+        // Step 3 - Update the user details (only update able fields) - 403 Forbidden
+        // Step 4 - Return the details of the user if updated - 200 OK
+        return ResponseEntity.ok("My Profile Updated");
     }
 }
